686c3ce6dcfd2b2e10c197d2589e4ebf0b82d17c
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-04-29 10:15:23 +0900

    Uncomment EFI check and add withefi to CMDLINE_LINE

--- buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
+++ buildroot/board/tcrpfriend/rootfs-overlay/root/boot.sh
@@ -1600,9 +1600,10 @@ function boot() {
 
     #CMDLINE_LINE+="skip_vender_mac_interfaces=0,1,2,3,4,5,6,7 "
 
-    #if [ "$EFIMODE" = "yes" ] && [ $(echo ${CMDLINE_LINE} | grep withefi | wc -l) -le 0 ]; then
-    #    CMDLINE_LINE+="withefi " && echo -en "\r$(msgwarning "$(TEXT "EFI booted system with no EFI option, adding withefi to cmdline")")\n"
-    #fi
+    #If EFI then add withefi to CMDLINE_LINE
+    if [ "$EFIMODE" = "yes" ] && [ $(echo ${CMDLINE_LINE} | grep withefi | wc -l) -le 0 ]; then
+        CMDLINE_LINE+="withefi " && echo -en "\r$(msgwarning "$(TEXT "EFI booted system with no EFI option, adding withefi to cmdline")")\n"
+    fi
 
     if [ "$(dmidecode -s system-manufacturer | grep -c VMware)" -eq 1 ]; then
         CMDLINE_LINE+="mev=vmware "
